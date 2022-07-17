6c3ee3bb8a764dbcd9f4b2ff58b303af9203fea8
saiprashanthts1995 <64998983+saiprashanthts1995@users.noreply.github.com>
2022-07-17 14:17:05 -0400

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 from app import index
 
 def test_route_index():
-    assert index() == 'This Sai Prashanth T S. Alright, Starting from the scratch'
+    assert index() == 'This Sai Prashanth T S. Alright, Starting from the scratch '
